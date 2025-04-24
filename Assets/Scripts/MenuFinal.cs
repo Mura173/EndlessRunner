ad63e07ee4e8165a479bd2db694be7540d5be7e8
vitorkolle <vitorpaeskolle@gmail.com>
2025-04-24 15:24:46 -0300

    tela final desenvolvida e funcional

---
+++ Assets/Scripts/MenuFinal.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuFinal : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void PlayAgain(){
+        SceneManager.LoadScene(1);
+    }
+}
