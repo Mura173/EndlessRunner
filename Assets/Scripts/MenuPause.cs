4624cc1bd00163add8f2b19a6257091f33ce80ad
vitorkolle <vitorkolle.dev@gmail.com>
2025-04-25 09:59:44 -0300

    menu feito da forma certa e funcionando

--- Assets/Scripts/MenuPause.cs
+++ Assets/Scripts/MenuPause.cs
@@ -3,20 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class MenuPause : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    bool isPaused = false;
     void Start()
     {
-        
+       pauseMenu.SetActive(false);
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)){
+
+            if (isPaused){
+                ResumeGame();
+            }
+
+            PauseGame();
+        }
     }
 
+    public void PauseGame(){
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
     public void ResumeGame(){
-        //definir a lógica para quando for voltar pro jogo
-
-        SceneManager.LoadScene(1);
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
     }
 
     public void ExitGame(){
